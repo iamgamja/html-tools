94208e127db37949f3e24ff0e5713d234febbc25
iamgamja <68046824+iamgamja@users.noreply.github.com>
2021-04-19 16:02:25 +0900

    Update true_calculator.js

--- true_calculator.js
+++ true_calculator.js
@@ -2,17 +2,29 @@
 // 사용 예시: true_calculator("123456789012345678901234567890+1")         => "123456789012345678901234567891"
 // 사용 예시: true_calculator("123456789012345678901234567890", "+", "1") => "123456789012345678901234567891"
 
+
+  function change_minus(s) {  return s.startsWith('-') ? s.slice(1) : '-'+s  }
+
   function true_calculator() {
     if (arguments.length === 1) {
-      if (/^-?\d+\+\d+$/.test(arguments[0])) {
+      if (/^\d+\+\d+$/.test(arguments[0])) { // 양수+양수
         var s1 = arguments[0].split('+')[0], p = '+', s2 = arguments[0].split('+')[1];
-      } else if (/^-?\d+-\d+$/.test(arguments[0])) {
+      } else if (/^-\d+\+\d+$/.test(arguments[0])) { // 음수+양수
+        var s1 = arguments[0].split('+')[0], p = '+', s2 = arguments[0].split('+')[1];
+        return change_minus(true_calculator(s1.slice(1), '-', s2))
+      } else if (/^\d+-\d+$/.test(arguments[0])) { // 양수-양수 (양수+음수)
         var s1 = arguments[0].split('-')[0], p = '-', s2 = arguments[0].split('-')[1];
+      } else if (/^-\d+-\d+$/.test(arguments[0])) { // 음수-양수 (음수+음수)
+        var s1 = '-'+arguments[0].split('-')[1], p = '-', s2 = arguments[0].split('-')[2];
+        console.log(s1, p, s2)
+        return change_minus(true_calculator(s1.slice(1), '+', s2))
       } else {
         return NaN;
       }
     } else if (arguments.length === 3) {
       var s1 = arguments[0], p = arguments[1], s2 = arguments[2];
+      if (s1.startsWith('-') && p === '+') return change_minus(true_calculator(s1.slice(1), '-', s2))
+      if (s1.startsWith('-') && p === '-') return change_minus(true_calculator(s1.slice(1), '+', s2))
     } else {
       return NaN;
     }
@@ -29,7 +41,6 @@
 
       // 만약 js로 정확하게 계산할 수 없다면, 직접 계산합니다.
       if (s2.length !== 1) return true_calculator((true_calculator(s1.slice(0, -1), '+', s2.slice(0, -1)) + s1.slice(-1)), '+', s2.slice(-1)) // s2가 한자리가 아니라면 -> 앞자리끼리 계산, 뒤에 붙이고 다시 계산.
-      if (s1.startsWith('-')) return true_calculator(s2, '-', s1.slice(1)) // 만약 s1이 음수라면 s2에서 s1을 빼기 ( -s1+s2 = s2-s1 )
       if (Number(s1.slice(-1)) + Number(s2) > 9) { // 마지막자리끼리 더했더니 9보다 크다 = 마지막 둘째자리(그 앞자리)에 1을 더한다
         return (true_calculator(s1.slice(0, -1), '+', '1') + String(parseInt(s1.slice(-1)) + parseInt(s2) - 10)).replace(/^0+(?=.)/, "")
       } else { // 아니면 그냥 뒷자리끼리만 계산
@@ -41,7 +52,7 @@
 
       // 만약 js로 정확하게 계산할 수 없다면, 직접 계산합니다.
       if (s2.length !== 1) return true_calculator((true_calculator(s1.slice(0, -1), '-', s2.slice(0, -1)) + s1.slice(-1)), '-', s2.slice(-1)) // s2가 한자리가 아니라면 -> 앞자리끼리 계산, 뒤에 붙이고 다시 계산.
-      if (s1.startsWith('-')) return '-' + true_calculator(s2, '+', s1.slice(1)) // 만약 s1이 음수라면 s2에서 s1을 뺀후 - 붙이기 ( -s1-s2 = -(s2+s1) )
+      if (Number(s1) < Number(s2)) return change_minus(true_calculator(s2, '-', s1))
       if (Number(s1.slice(-1)) - Number(s2) < 0) { // 마지막자리끼리 뺐더니 0보다 작다 = 마지막 둘째자리에 1을 뺀다
         return (true_calculator(s1.slice(0, -1), '-', '1') +''+ (Number(s1.slice(-1)) - Number(s2) + 10)).replace(/^0+(?=.)/, "")
       } else { // 아니면 그냥 뒷자리끼리만 계산
